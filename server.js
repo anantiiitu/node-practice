ffd98309726ad08687cbb7ed7bf717c33c15ea73
Anant <anantiiitu@gmail.com>
2019-07-21 20:01:48 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,94 +1,50 @@
-var trophies = [
-    {
-        id: 1,
-        trophy:"Serie A",
-        team: "Juventus",
-        year: 2014
-    },
-    {
-        id: 2,
-        trophy:"La Liga",
-        team: "Real Madrid",
-        year: 2015
-    },
-    {
-        id: 3,
-        trophy:"EPL",
-        team: "Manchester City",
-        year: 2016
-    }
+var sqlite3 = require('sqlite3');
 
-];
+var db = new sqlite3.Database(':memory:');
 
-var express = require('express'); //npm install express --save 
-var bodyParser = require('body-parser'); //npm install --save body-parser
-var sqlite3 = require('sqlite3'); //npm install sqlite3 --save
-var app = express();
+db.serialize(function(){
+    db.run('CREATE TABLE Contacts(fname TEXT, lname TEXT, age INTEGER)');
 
-var db = new sqlite3.Database('trophies.db');
-var port = 3000;
+    db.run('INSERT INTO Contacts VALUES("Anant", "Kabra", 22)');
+    db.run('INSERT INTO Contacts VALUES("Tanmay", "Maheshwari", 12)');
+    db.run('INSERT INTO Contacts VALUES("Billy", "Russo", 32)');
 
-app.listen(port, function(){
-    console.log('Express App listening to port ' + port);
-});
+    db.all('SELECT * FROM Contacts', processRows);
+    db.each('SELECT * FROM Contacts', processRows1);
+    db.each('SELECT * FROM Contacts WHERE lname = "Russo"', processRows1);
 
-app.use(bodyParser.urlencoded({extended : true})) ;
-
-app.post('/trophies', function(req, res){
-    console.log('Insert a new trophy: ' + req.body.trophy) ;
-    db.run('INSERT INTO trophies VALUES(?, ?, ?, ?)', [req.body.trophy, req.body.team, req.body.year, req.body.id], function(err, row){
+    var firstName = 'Anant' ;
+    db.get('SELECT * FROM Contacts WHERE fname = ?', [firstName], function(err, rows){
+        console.log("Get Anant's age : ");
         if(err){
             console.log('Error : ' + err.message);
         }
         else{
-            res.send('Inserted trophy by id: ' + this.lastID);
+            console.log(rows.age);
         }
     });
+
 });
 
-app.get('/', function(req, res){
-    res.send('Get request received at "/"');
-});
-
-app.get('/trophies', function(req, res){
-    if(req.query.year){
-        db.all('SELECT * FROM trophies WHERE year = ?', [req.query.year], function(err, row){
-            if(err){
-                res.send('Error : ' + err.message);
-            }
-            else{
-                console.log('Return a list of trophies from the year : ' + req.query.year);
-                res.json(row) ;
-            }
-        });
-        // localhost:3000/trophies?year=2014
+function processRows(err, rows){
+    if(err){
+        console.log('Error : ' + err.message);
     }
     else{
-        db.all('SELECT * FROM trophies', function processRows(err, row){
-            if(err){
-                res.send('Error : ' + err.message);
-            }
-            else{
-                for(var i=0 ; i<row.length ; i++)
-                {
-                    console.log(row[i].trophy);
-                }
-                res.json(row);
-            }
-        });
+        for(var i=0 ; i<rows.length; i++)
+        {
+            console.log(rows[i].name);
+        }
     }
-});
+}
 
-app.get('/trophies/:id', function(req, res){
-    console.log('Return the trophies by id : ' + req.params.id);
-    db.get('SELECT * FROM trophies WHERE id = ?', [req.params.id], function(err, row){
-        if(err){
-            console.log(err.message);
-        }
-        else{
-            res.json(row);
-        }
-    });
-    //res.send('Return trophy by ID: ' + req.params.id);
-});
+function processRows1(err, rows){
+    if(err){
+        console.log('Error : ' + err.message);
+    }
+    else{
+        console.log(rows.name);
+    }
+}
 
+db.close();
